7827c74f010d686ae8b13bd7823960f38ae97fd2
x-zen <jdharris.x@gmail.com>
2019-04-24 03:10:37 -0500

    Server listening

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code away!
+const express = require('express'); // import the express package
+
+//const apiRoutes = require('./routing/api/apiRoutes'); // import the post endpoints
+
+const server = express(); // creates the server
+
+server.use(express.json()); // add this to make POST and PUT work
+
+
+// handle request to the root of the API
+server.get('/', (req, res) => {
+  res.send('Welcome to The Node Blog API | By: x-zen');
+});
+
+// assigns the route for api endpoints
+//server.use('/api', apiRoutes);
+
+// watch for connections on port 3000
+server.listen(5000, () =>
+  console.log('\n** Server running on http://localhost:5000 **')
+);
