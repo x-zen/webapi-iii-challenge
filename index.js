bea0c47eb38edc1ce861040b204ecaea25dbf051
x-zen <jdharris.x@gmail.com>
2019-04-25 13:55:49 -0500

    GREET

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ server.use(express.json()); // add this to make POST and PUT work
 
 // handle request to the root of the API
 server.get('/', (req, res) => {
-  res.send('Welcome to The Node Blog API | By: x-zen');
+  const homeGreeting = process.env.GREET || 'Server up, no greeting available';
+  res.send(`${homeGreeting}`);
 });
 
 // assigns the route for api endpoints
